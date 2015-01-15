a23a960b69f8dbdcfa0f8426370535057ad7b653
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-01-15 00:18:43 -0800

    Better backtracking algorithm

--- sudoku.hs
+++ sudoku.hs
@@ -4,14 +4,16 @@ import Control.Applicative
 import Control.Monad
 import System.Environment
 import Data.Char
+import Data.Maybe
+import Data.Ord
+import Data.Monoid
 import qualified Data.Map as Map
 
 newtype Val = V Int deriving (Eq)
-newtype Row = R Int deriving (Eq, Ord)
-newtype Col = C Int deriving (Eq, Ord)
+newtype Row = R Int deriving (Eq, Ord, Show)
+newtype Col = C Int deriving (Eq, Ord, Show)
 type Loc = (Row, Col)
 type Status = [Val]
-type Cell = (Loc, Status)
 type Board = Map.Map Loc Status
 
 vals = map V [1..9]
@@ -63,53 +65,54 @@ relatedLocs l = filter (related l) locs
 
 eliminate :: Val -> Loc -> Board -> Board
 eliminate val loc b =
-  if length old == 2 && length new == 1
-    then markAsCertain (head new) loc b'
+  if uncertain s && certain s'
+    then set b' loc $ head s'
     else b'
-  where old = b Map.! loc
-        new = old \\ [val]
-        b'  = Map.insert loc new b
+  where s = b Map.! loc
+        s' = s \\ [val]
+        b'  = Map.insert loc s' b
 
-markAsCertain :: Val -> Loc -> Board -> Board
-markAsCertain v loc b =
+set :: Board -> Loc -> Val -> Board
+set b loc v =
   foldr (eliminate v) (Map.insert loc [v] b) $ relatedLocs loc
 
-markGivenCertainties :: Board -> Board
-markGivenCertainties b =
-  foldr (\(loc, status) -> markAsCertain (head status) loc) emptyBoard givens
-  where givens = filter (\(loc, status) -> length status == 1) $ Map.assocs b
+uncertain :: Status -> Bool
+uncertain s = length s > 1
+
+certain :: Status -> Bool
+certain s = length s == 1
+
+setGivens :: Board -> Board
+setGivens board = foldr (\(l, s) -> \b -> set b l $ head s) emptyBoard givens
+  where givens = filter (certain . snd) $ Map.assocs board
 
 solved :: Board -> Bool
-solved = all (==1) . map length . Map.elems
+solved = all ((==1) . length) . Map.elems
 
 contradictory :: Board -> Bool
 contradictory = any null . Map.elems
 
-possibilities :: Board -> [Board]
-possibilities b = concat $ map (possibleChanges b) (Map.assocs b)
+guesses :: Board -> [Board]
+guesses b = case fu of
+              Nothing -> []
+              Just (l, s) -> filter (not . contradictory) (set b l <$> s)
+  where fu = find ((>1) . length . snd) $ Map.assocs b
 
-possibleChanges :: Board -> Cell -> [Board]
-possibleChanges b (loc, status) =
-  if length status > 1
-    then filter (not . contradictory) $
-         markAsCertain <$> status <*> [loc] <*> [b]
-    else []
 
-findSolution :: [Board] -> Maybe Board
-findSolution [] = Nothing
-findSolution bs =
-  case find solved bs of
-    Just s  -> Just s
-    Nothing -> findSolution $ bs >>= possibilities
+solutions :: Board -> [Board]
+solutions b = if solved b
+                then [b]
+                else guesses b >>= solutions
 
 solve :: Board -> Maybe Board
-solve b = findSolution [markGivenCertainties b]
+solve = listToMaybe . solutions
 
 main = do
    args <- getArgs
    input <- readFile $ head args
    let board = readBoard input
-   let output = case solve board of
-                  Just solution -> showBoard solution
-                  Nothing       -> "Unsolvable"
-   putStr output
+   putStrLn . showBoard $ board
+   let solution = solve . setGivens $ board
+   case solution of
+      Nothing -> putStrLn "No Solutions"
+      Just s  -> putStrLn . showBoard $ s
